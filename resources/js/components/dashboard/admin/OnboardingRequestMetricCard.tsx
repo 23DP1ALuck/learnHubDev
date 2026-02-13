11ef61d596476cc3769b1fc59903944de6367362
t0mmyq <a231660al@rvt.lv>
2026-02-13 11:52:46 +0200

    Icons color fix

--- resources/js/components/dashboard/admin/OnboardingRequestMetricCard.tsx
+++ resources/js/components/dashboard/admin/OnboardingRequestMetricCard.tsx
@@ -5,9 +5,9 @@ type OnboardingRequestMetricCardProps = {
     count : number
 }
 const iconsByStatus = {
-    pending : <Clock fill="blue" stroke="white"/>,
-    approved : <CheckCircleIcon fill="green" stroke="white"/>,
-    rejected : <CircleX fill="red" stroke="white"/>
+    pending : <Clock className="fill-blue-400 text-white"/>,
+    approved : <CheckCircleIcon className="fill-green-400 text-white"/>,
+    rejected : <CircleX className="fill-red-400 text-white"/>
 }
 export const OnboardingRequestMetricCard = ({onboardingRequestType, count}:OnboardingRequestMetricCardProps) => {
     return <div className="flex flex-col bg-white rounded-xl p-6 gap-5 border border-sidebar-border/70 dark:border-sidebar-border">
