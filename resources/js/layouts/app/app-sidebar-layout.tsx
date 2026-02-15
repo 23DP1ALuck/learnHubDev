351ac7e00cac33e8621f01ee6f7ac80f0342427e
t0mmyq <a231660al@rvt.lv>
2026-02-15 18:28:17 +0200

    Overflow bug fix

--- resources/js/layouts/app/app-sidebar-layout.tsx
+++ resources/js/layouts/app/app-sidebar-layout.tsx
@@ -17,7 +17,7 @@ export default function AppSidebarLayout({
             {auth?.user.role === 'admin' ? <Sidebar/> : <DashboardSidebar/>}
             <AppContent variant="sidebar" className="overflow-hidden">
                 <AppSidebarHeader breadcrumbs={breadcrumbs} />
-                <div className="flex flex-1 flex-col">
+                <div className="flex flex-1 flex-col overflow-y-hidden">
                     {children}
                 </div>
             </AppContent>
